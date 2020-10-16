3234c40bf2e23de1a7f1455ab0beb0fee94f923c
NahrulK <khayattullah73@gmail.com>
2020-10-16 10:03:33 +0800

    Menambahkan list

--- index.php
+++ index.php
@@ -11,5 +11,10 @@
 
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae illo deleniti aut eum voluptate, assumenda eos illum? Amet molestiae eius blanditiis sunt, mollitia maiores in quasi illo sapiente nam expedita.</p>
     
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
+
 </body>
 </html>
